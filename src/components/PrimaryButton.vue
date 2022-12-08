14a876f1c11e8be032a387a395a414abe51a2ed6
longnt80 <long.nguyen@weployapp.com>
2022-12-08 14:56:27 +1100

    Add initial PrimaryButton component - Modify the default App component to display button component - Add minimal stylings for PrimaryButton component

---
+++ src/components/PrimaryButton.vue
@@ -0,0 +1,43 @@
+<script setup>
+const props = defineProps({
+  size: String,
+  badgeCount: Number
+});
+</script>
+
+<template>
+  <button>
+    <div class="icon">
+      <slot name="icon"></slot>
+    </div>
+    <slot></slot>
+    <div class="badge">{{ props.badgeCount }}</div>
+  </button>
+</template>
+
+<style scoped>
+  button {
+    font-family: Inter, sans-serif;
+    display: flex;
+    gap: 12px;
+    justify-content: center;
+    align-items: center;
+    height: 40px;
+    font-size: 13px;
+    padding: 10px;
+    line-height: 20px;
+    font-weight: 500;
+    color: #fff;
+    background: #33B679;
+    border: none;
+    border-radius: 8px;
+  }
+
+  .icon {
+    display: flex;
+  }
+
+  .badge {
+    display: flex;
+  }
+</style>
