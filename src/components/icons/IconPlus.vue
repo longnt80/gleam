d371ff12d6e4fedb7263f043a90367e7b3707393
longnt80 <long.nguyen@weployapp.com>
2022-12-08 14:54:18 +1100

    Add plus icon component

---
+++ src/components/icons/IconPlus.vue
@@ -0,0 +1,13 @@
+<template>
+  <svg
+    width="20"
+    height="20"
+    class="icon"
+    viewBox="0 0 20 20"
+    fill="currentColor"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+
+    <path d="M10 5v10m-5-5h10" stroke="currentColor" stroke-width="1.3" stroke-linecap="round" stroke-linejoin="round"/>
+  </svg>
+</template>
